65010bf54f7bc9f4e80af652d6bdc73ed51693c4
leehojin-93 <hogenie93@gmail.com>
2021-06-08 16:26:37 +0900

    nameChange

---
+++ src/stage01/Q04_10172.java
@@ -0,0 +1,19 @@
+package stage01;
+
+public class Q04_10172 {
+	public static void main(String[] args) {
+
+		// 아래 예제와 같이 개를 출력하시오.
+		/*
+		|\_/|
+		|q p|   /}
+		( 0 )"""\
+		|"^"`    |
+		||_/=\\__| 
+		 */
+
+		System.out.println("|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|");
+
+	}
+
+}
