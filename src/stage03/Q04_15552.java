63347be5cd0af1b73bc58af573bc8ba5dc9e7188
leehojin-93 <hogenie93@gmail.com>
2021-06-17 19:03:20 +0900

    21. 06. 17. Thu) 19:01 No.15552 failCodeFix

--- src/stage03/Q04_15552.java
+++ src/stage03/Q04_15552.java
@@ -19,23 +19,23 @@ public class Q04_15552 {
 		int T = Integer.parseInt(bfr.readLine()); // 입력값으로 반복 횟수 설정
 
 		for ( int i = 0; i < T; i++ ) {
+			/*
 			sT  = new StringTokenizer(bfr.readLine()); // BufferReader.redLine() 로 입력받은 값을 StringTokenizer 에 대입
 
 			// sT.nextToken() --> 입력 받은 String 값을 공백 단위로 나눠서 순서대로 출력 공백+1 만큼의 배열이 생성 되는 느낌 == .split(" ")
 			int A = Integer.parseInt( sT.nextToken() ); // 첫 번째 String 값을 int 형으로 변환
 			int B = Integer.parseInt( sT.nextToken() ); // 두 번째 String 값을 int 형으로 변환
 
-			bfw.write(A + B + "\n");
-			//			bfw.flush(); --> 한줄 입력 후 출력 하려면 .flush() 작성
-
-			/* 실패
-			String text = bfr.readLine();
-			String[] word = text.split(" ");
-			int A = Integer.parseInt(word[0]);
-			int B = Integer.parseInt(word[1]);
-			bfw.write(A + B);
-			bfw.newLine();
+			bfw.write(A + B + "\n"); // "\n" 미작성시 출력 문자 오류 발생
+			//			bfw.flush(); --> 한줄 입력 후 출력 하려면 .flush() 작성 --> Buffer 에 1회씩 저장 후 출력을 반복하기 때문에 총 반복 횟수만큼의 출력시간이 늘어남 --> 미작성시 반복 횟수만큼 저장후 한번에 출력
 			 */
+
+			// 수정 --> 오류없음
+			//			String text = bfr.readLine();
+			String[] word = bfr.readLine().split(" "); // 입력값을 공백으로 나눠서 word 배열에 대입
+			int A = Integer.parseInt(word[0]); // 공백 전(첫 번째 값 [0]) 입력 값을 int 형으로 변환후 A에 대입
+			int B = Integer.parseInt(word[1]); // 공백 후(두 번째 값 [1]) 입력 값을 int 형으로 변환후 A에 대입
+			bfw.write(A + B + "\n"); // "\n" 미작성시 출력 문자 오류 발생
 
 		}
 
