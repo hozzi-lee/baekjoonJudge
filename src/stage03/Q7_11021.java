d1279568e603a9b8104aa174bcb6596e87ad45a7
leehojin-93 <hogenie93@gmail.com>
2021-06-17 22:11:43 +0900

    21. 06. 17. Thu) 22:11 No.11021

---
+++ src/stage03/Q7_11021.java
@@ -0,0 +1,24 @@
+package stage03;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Q7_11021 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bfw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int T = Integer.parseInt(bfr.readLine());
+		for ( int i = 0; i < T; i++ ) {
+			String[] word = bfr.readLine().split(" ");
+			int A = Integer.parseInt(word[0]);
+			int B = Integer.parseInt(word[1]);
+			bfw.write("Case #" + (i + 1) + ": " + (A + B) + "\n");
+		}	
+		bfw.close();
+		bfr.close();
+	}
+
+}
